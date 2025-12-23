eff69e0a9ea571529d5331e1afebc5c52f5135a4
s4midev <astingraye@proton.me>
2025-12-23 02:28:58 +0000

    init commit actual svelte project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import favicon from "$lib/assets/cold.png";
+  import "../style.css";
+
+  let { children } = $props();
+  
+</script>
+
+export const prerender = true;
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <title>coldseason</title>
+</svelte:head>
+
+{@render children()}
