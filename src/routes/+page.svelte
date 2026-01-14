aa70d6337865ea41197c2399308d7f02f2d526f8
s4midev <astingraye@proton.me>
2026-01-14 20:20:02 +0000

    add factory accident

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
+<script>
+  import { browser } from "$app/environment";
+  import "$lib/assets/coldseason.css";
+</script>
+
 <div>
+  <a
+    class="falink"
+    style="position: absolute; top: 2rem; right: 2rem;"
+    href="/factoryaccident"
+    data-sveltekit-preload-data="off">(F[]CT[]RY []CC[]D[]NT)</a
+  >
   <h1>coldseason</h1>
   <h1 class="sub">this music is sentient and judging you</h1>
-  {#each [
-    { name: "spotify", url: "https://open.spotify.com/artist/4NWVxhAQhzcNXxiAU79FnL" },
-    { name: "apple music", url: "https://music.apple.com/gb/artist/coldseason/1860686513" },
-    { name: "deezer", url: "https://www.deezer.com/en/artist/68923822" },
-    { name: "amazon music", url: "https://music.amazon.co.uk/artists/B07TYS3TLF/coldseason" },
-    { name: "bandcamp", url: "https://mcoldseason.bandcamp.com"}
-    ] as { name, url }, i}
+  {#each [{ name: "spotify", url: "https://open.spotify.com/artist/4NWVxhAQhzcNXxiAU79FnL" }, { name: "apple music", url: "https://music.apple.com/gb/artist/coldseason/1860686513" }, { name: "deezer", url: "https://www.deezer.com/en/artist/68923822" }, { name: "amazon music", url: "https://music.amazon.co.uk/artists/B07TYS3TLF/coldseason" }, { name: "bandcamp", url: "https://mcoldseason.bandcamp.com" }, { name: "merch", url: "https://elasticstage.com/coldseason" }] as { name, url }, i}
     <a href={url} target="_blank">
       <h1 class={`${i === 0 ? "break" : ""} link`}>{name}</h1>
     </a>
